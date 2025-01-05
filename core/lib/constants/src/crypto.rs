5f1f1b29ba0c2830fdda63dca3a497cdd0a295d5
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2025-01-05 15:47:16 +0300

    Update crypto.rs
    String 8: was 24. now 21

--- core/lib/constants/src/crypto.rs
+++ core/lib/constants/src/crypto.rs
@@ -5,4 +5,4 @@ pub const ROOT_TREE_DEPTH: usize = 256;
 
 /// To avoid DDoS we limit the size of the transactions size.
 /// TODO(X): remove this as a constant and introduce a config.
-pub const MAX_ENCODED_TX_SIZE: usize = 1 << 24;
+pub const MAX_ENCODED_TX_SIZE: usize = 1 << 21;
